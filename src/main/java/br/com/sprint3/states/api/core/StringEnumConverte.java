c16617b396a3da4c5045d4c2a574f13c941c7cc1
Lucas <lucasmagno695@gmail.com>
2022-11-13 23:32:44 -0300

    Implementando Bad Request para valores inválidos na filtragem por região, adicioando classe Handler e atualizando o arquivo yaml, colocando example nos schemas e adicionando o erro 400 no endpoint de listagem

--- src/main/java/br/com/sprint3/states/api/core/StringEnumConverte.java
+++ src/main/java/br/com/sprint3/states/api/core/StringEnumConverte.java
@@ -5,13 +5,9 @@ import org.springframework.core.convert.converter.Converter;
 
 public class StringEnumConverte implements Converter<String, Regiao> {
 
-    @Override
-    public Regiao convert(String source) {
-        try {
-            return Regiao.valueOf(source.toUpperCase());
-        } catch (IllegalArgumentException e) {
-            return null;
-        }
-    }
+	@Override
+	public Regiao convert(String source) {
+		return Regiao.valueOf(source.toUpperCase());
 
+	}
 }
